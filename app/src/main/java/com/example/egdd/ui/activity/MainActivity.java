6662436d3431186ef10afd0555d44ddf4f1e077c
e <472741357@qq.com>
2020-09-06 20:07:47 +0800

    yj

--- app/src/main/java/com/example/egdd/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/egdd/ui/activity/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         initFragment(); //设置主fragment
+
     }
 
     private void initFragment() {
