6f8347e1895887c72084a9e93231d9717ee71997
。。。。 <121>
2020-09-06 20:40:13 +0800

    chenxiao

--- app/src/main/java/com/example/egdd/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/egdd/ui/activity/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         initFragment(); //设置主fragment
         Log.e("11", "onCreate: "+"上传" );
         LogUtils.e("wangys");
+        Log.e("333", "onCreate: "+"我是你爹" );
 
     }
 
